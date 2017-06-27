8569f5fa233e835759fd2ef4031c63ad987fbf65
Winkeltje84 <bwinkel@gmail.com>
2017-06-27 15:21:05 +0200

    get rid of Test / first setup PostIndex page

--- src/index.js
+++ src/index.js
@@ -2,29 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom';
 // BrowserRouter interacts with the browser history and decides what to do
 // Route determines what is shown when a certain url is requested
 
-import App from './components/app';
+import PostIndex from './components/post_index'
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-  render() { return <div>Hello!</div>}
-}
-
-class Goodbye extends React.Component {
-  render() { return <div>Goodbye!</div> }
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/hello" component={Hello}/>
-        <Route path="/goodbye" component={Goodbye}/>
+        <Route path="/" component={PostIndex}/>
       </div>
     </BrowserRouter>
   </Provider>
