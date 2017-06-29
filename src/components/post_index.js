d8bd35a1d3cd9d7a4e0bb8ab908e9e9d6d994f70
Winkeltje84 <bwinkel@gmail.com>
2017-06-29 14:57:00 +0200

    itterating over objects with lodash & showing titles of posts on page

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchPosts } from '../actions/index'
+import _ from 'lodash'
 
 class PostIndex extends Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      )
+    })
+  }
+
   render() {
-    console.log(this.props.posts)
     return(
-      <div>PostIndex</div>
+      <div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
+      </div>
     )
   }
 }
