d0e1167ea4dc84314b0f807f085962766cdadda2
Winkeltje84 <bwinkel@gmail.com>
2017-06-29 15:34:43 +0200

    creating a post_new page & using Switch of react-router-dom to be able to switch between them

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // BrowserRouter interacts with the browser history and decides what to do
 // Route determines what is shown when a certain url is requested
 import promise from 'redux-promise'
 
 import PostIndex from './components/post_index'
+import PostNew from './components/post_new'
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
@@ -16,7 +17,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostIndex}/>
+        <Switch>
+          <Route path="/posts/new" component={PostNew}/>
+          <Route path="/" component={PostIndex}/>
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
