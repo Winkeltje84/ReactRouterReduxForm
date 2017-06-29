db67ef633dbc14b05b513e7beb4ebde2f60e5793
Winkeltje84 <bwinkel@gmail.com>
2017-06-29 15:44:00 +0200

    added link to send us back from post_new to index page

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
-
+import { Link } from 'react-router-dom'
 class PostNew extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/">
+            Back to Index
+          </Link>
+        </div>
         PostNew!
       </div>
     )
