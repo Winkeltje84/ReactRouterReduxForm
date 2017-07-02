ec7c47a887c16e4dc5fc8f091fc7297ab7cc5de1
Winkeltje84 <bwinkel@gmail.com>
2017-07-02 16:06:09 +0200

    added a fetchPost action that fetches a post & console logs it when visiting the post_show page

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import { fetchPost } from '../actions/index';
+import { connect } from 'react-redux';
 
 class PostShow extends Component {
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    this.props.fetchPost(id);
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +17,4 @@ class PostShow extends Component {
   };
 }
 
-export default PostShow
+export default connect(null, { fetchPost })(PostShow);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,15 @@
-import { FETCH_POSTS } from '../actions/index'
+import { FETCH_POSTS, FETCH_POST } from '../actions/index'
 import _ from 'lodash'
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case FETCH_POST:
+      const post = action.payload.data;    // getting data from api & setting it to post
+      const newState = { ...state };       // creating a new state
+      newState[post.id] = post;            // adding post to the state with key of its id
+      console.log(newState);
+      return newState;
+
     case FETCH_POSTS:
       const new_state =  _.mapKeys(action.payload.data, 'id')
       // this lodash function iterates over object and places its 'id' key value
